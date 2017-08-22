cdf94d254494e64462f2f94e60b64e9a02742e5e
nabil2199 <nabil2199@gmail.com>
2017-08-22 19:17:27 +0200

    * function implementation * file writing

--- ConvertExcelCsv.py
+++ ConvertExcelCsv.py
@@ -9,10 +9,15 @@ import openpyxl, os, sys
 #workbook=openpyxl.load_workbook(wbpath, data_only=True)
 #workbook.get_sheet_names()
 
-workbook=openpyxl.load_workbook('C:\\Users\\NXLX8474\\Desktop\\todo20170824\\lotb5\\lotb5.xlsm', data_only=True)
+#load and initialise destination csv file
+csvfile = open('C:\\Users\\NXLX8474\\Desktop\\todo20170824\\lotb5\\lotb5test.csv', 'w')
+csvfile.write('upn,FirstName,LastName,SipAddress,EmailAddress,TelURI,Extension,VoicePolicy,Tactical,VoiceMail,Desk,DisplayNumber\n')
+
+
+workbook = openpyxl.load_workbook('C:\\Users\\NXLX8474\\Desktop\\todo20170824\\lotb5\\lotb5.xlsm', data_only=True)
 workbook.get_sheet_names()
-sheet=workbook.get_sheet_by_name('Lot A - User Information')
-sheetsize=sheet.max_row
+sheet = workbook.get_sheet_by_name('Lot A - User Information')
+sheetsize = sheet.max_row
 
 #def getUserInfoSheetSize(workbookfile):
 #    size=1
@@ -23,7 +28,7 @@ sheetsize=sheet.max_row
 #    return size
 
 def loadColumn(columnindex):
-    valuelist=[]
+    valuelist = []
     i=1
     while (i < sheetsize):
         cellcoordinate=columnindex + str(i)
@@ -32,31 +37,72 @@ def loadColumn(columnindex):
         i=i+1
     return valuelist
 
-def loadUPN(sheetsize):
+def loadUPN():
     upnlist = loadColumn("E")
     return upnlist
 
-def loadSIP(sheetsize):
+def loadSIP():
     upnlist = loadColumn("F")
     return upnlist
 
-def loadFName(sheetsize):
-    fnamelist = loadColumn("E")
+def loadFName():
+    fnamelist = loadColumn("B")
     return fnamelist
 
-def loadLName(sheetsize):
-    lnamelist = loadColumn("F")
+def loadLName():
+    lnamelist = loadColumn("C")
     return lnamelist
 
-upns=loadUPN(sheetsize)
-sips=loadSIP(sheetsize)
+def loadEMail():
+    emaillist = loadColumn("H")
+    return emaillist
 
+def loadLTelUri():
+    telurilist = loadColumn("I")
+    return telurilist
 
-for i in range(0,sheetsize-1):
-    if upns[i] != None and sips[i] != None:
-        print(upns[i])
-        print(sips[i])
+def loadExtension():
+    extensionlist = loadColumn("J")
+    return extensionlist
 
-def initFile():
-    return 5
+def loadVoicePolicy():
+    voicepolicylist = loadColumn("M")
+    return voicepolicylist
 
+def loadTactical():
+    tacticallist = loadColumn("S")
+    return tacticallist
+
+def loadVoiceMail():
+    voicemaillist = loadColumn("V")
+    return voicemaillist
+
+def loadDesk():
+    desklist = loadColumn("AD")
+    return desklist
+
+def loadDisplayNumber():
+    displaynumberlist = loadColumn("R")
+    return displaynumberlist
+
+bupnlist = loadUPN()
+fnamelist = loadFName()
+lnamelist = loadLName()
+siplist = loadSIP()
+emaillist = loadEMail()
+telurilist = loadLTelUri()
+extensionlist = loadExtension()
+voicepolicylist = loadVoicePolicy()
+tacticallist = loadTactical()
+voicemaillist = loadVoiceMail()
+desklist = loadDesk()
+displaynumberlist = loadDisplayNumber()
+
+for i in range(1,sheetsize-1):
+    if bupnlist[i] != None and bupnlist[i] != 0:
+        line = str(bupnlist[i]) + ',' + str(fnamelist[i]) + ',' + str(lnamelist[i]) + ',' + str(siplist[i]) + ',' + str(emaillist[i]) + ',' + str(telurilist[i]) + ',' + str(extensionlist[i]) + ',' + str(voicepolicylist[i]) + ',' + str(tacticallist[i]) + ',' + str(voicemaillist[i]) + ',' + str(desklist[i]) + ',' + str(displaynumberlist[i] + '\n')
+        csvfile.write(line)
+        print(i)
+        print(line)
+
+csvfile.close()
