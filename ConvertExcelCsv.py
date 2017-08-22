e7e96f1a6ee6f59113439cf0b3ce085a1ba7a081
nabil2199 <nabil2199@gmail.com>
2017-08-22 13:34:00 +0200

    argument

--- ConvertExcelCsv.py
+++ ConvertExcelCsv.py
@@ -1,1 +1,10 @@
-import openpyxl
+#! python3
+
+import openpyxl, os, sys
+
+wbpath=sys.argv[1]
+currentworkdirectory=os.getcwd()
+
+print(wbpath)
+workbook=openpyxl.load_workbook(wbpath, data_only=True)
+workbook.get_sheet_names()
